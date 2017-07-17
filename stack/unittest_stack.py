781a683cfbfd96cc957fb228d6ff6bc88ab05021
Maryana <maryana.stlm@.com>
2017-07-17 21:30:47 +0300

    add few test cases for Stack class

--- stack/unittest_stack.py
+++ stack/unittest_stack.py
@@ -1,15 +1,36 @@
-import stack
+from stack import Stack
 import unittest
 
-s = Stack()
-s.push(1)
-
 class TestMyStack(unittest.TestCase):
 
-	def test_stack_empty(self):
-		self.assertEqual(s.peek(), 1)
-		
-class TestMyStack(unittest.TestCase):
+    def test_init(self):
+        empty_stack = Stack()
+        filled_stack = Stack([1, 2, 3])
+
+        self.assertEqual(empty_stack.get_items(), [])
+        self.assertEqual(empty_stack.size(), 0)
+
+        self.assertEqual(filled_stack.get_items(), [1, 2, 3])
+        self.assertEqual(filled_stack.size(), 3)
+
+    def test_is_empty(self):
+        empty_stack = Stack()
+        filled_stack = Stack([1, 2, 3])
+
+        self.assertTrue(empty_stack.isEmpty())
+        self.assertFalse(filled_stack.isEmpty())
+
+    def test_push(self):
+        stack = Stack()
+
+        for i in range(10):
+            stack.push(i)
+
+        self.assertEqual(stack.get_items(), [0,1,2,3,4,5,6,7,8,9])
+        self.assertEqual(stack.size(), 10)
+
+    '''
+
     def test_peek(self):
         self.assertEqual(s.peek(), 1)
 
@@ -18,6 +39,7 @@ class TestMyStack(unittest.TestCase):
 
     def test_pop(self):
         self.assertEqual(s.pop(), 3)
+    '''
 	
 if __name__ == '__main__':
     unittest.main()
