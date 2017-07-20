26574ebfe501dcbbd61d1f1904c99af372709dab
Maryana <maryana.stlm@.com>
2017-07-20 20:27:59 +0300

    add Set class

---
+++ set/set.py
@@ -0,0 +1,48 @@
+import copy
+
+class Set:
+    def __init__(self, initial = []):
+       unique_list = []
+       for i in initial:
+           if i not in unique_list:
+               unique_list.append(i)
+       self.__items = unique_list
+
+    def get_items(self):
+        return copy.deepcopy(self.__items)
+
+    #union
+    def __add__(self, other):
+        new_initial = self.__items + other.__items
+        return Set(new_initial)
+
+    def intersection(self, other):
+        intersection_list = []
+        for i in self.__items:
+            if i in other.__items:
+                intersection_list.append(i)
+        return Set(intersection_list)
+
+    #difference
+    #does not working
+    '''def __sub__(self, other):
+        new_initial = self.__items - other.__items
+        return Set(new_initial)'''
+
+    def difference(self, other):
+        difference_list = []
+        for i in self.__items:
+            if i not in other.__items:
+                difference_list.append(i)
+        return Set(difference_list)
+
+    def is_subset(self, other):
+        subset = True
+        for i in self.__items:
+            if i not in other.__items:
+                subset = False
+        return subset
+
+if __name__ == '__main__':
+
+
